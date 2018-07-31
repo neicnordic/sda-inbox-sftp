c53c19c2f76d91caa29a8ae2c5bb78ad4fac8003
Dmytro Titov <dmytro.titov@gmail.com>
2018-07-31 11:55:15 +0200

    Change IOException to RestClientException, cover it with a test.

--- src/test/java/se/nbis/lega/inbox/sftp/CredentialsProviderTest.java
+++ src/test/java/se/nbis/lega/inbox/sftp/CredentialsProviderTest.java
@@ -4,9 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestClientException;
 import se.nbis.lega.inbox.pojo.Credentials;
+import se.nbis.lega.inbox.pojo.KeyAlgorithm;
+import se.nbis.lega.inbox.pojo.PasswordHashingAlgorithm;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -21,10 +25,16 @@ public class CredentialsProviderTest extends InboxTest {
     private CredentialsProvider credentialsProvider;
 
     @Test
-    public void getCredentials() throws IOException, URISyntaxException {
+    public void getCredentialsSuccess() throws IOException, URISyntaxException {
         Credentials credentials = credentialsProvider.getCredentials(username);
         assertEquals(passwordHash, credentials.getPasswordHash());
         assertEquals(pubKey, credentials.getPublicKey());
+    }
+
+    @Test(expected = RestClientException.class)
+    public void getCredentialsFail() throws IOException, URISyntaxException {
+        mockCEGAEndpoint(username, password, PasswordHashingAlgorithm.BLOWFISH, KeyAlgorithm.RSA, HttpStatus.BAD_REQUEST);
+        credentialsProvider.getCredentials(username);
     }
 
     @Autowired
