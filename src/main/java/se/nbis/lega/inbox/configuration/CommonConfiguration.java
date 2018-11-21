7833bdbd000b6a531e94598f36bd22d1091beff1
Dmytro Titov <dmytro.titov@gmail.com>
2018-11-21 11:41:22 +0100

    Create bucket if not exists.

--- src/main/java/se/nbis/lega/inbox/configuration/CommonConfiguration.java
+++ src/main/java/se/nbis/lega/inbox/configuration/CommonConfiguration.java
@@ -1,5 +1,10 @@
 package se.nbis.lega.inbox.configuration;
 
+import com.amazonaws.util.StringUtils;
+import io.minio.MinioClient;
+import io.minio.errors.InvalidEndpointException;
+import io.minio.errors.InvalidPortException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,9 +15,39 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class CommonConfiguration {
 
+    private String s3Host;
+    private String s3AccessKey;
+    private String s3SecretKey;
+    private boolean useSSL;
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public MinioClient minioClient() throws InvalidPortException, InvalidEndpointException {
+        return StringUtils.isNullOrEmpty(s3AccessKey) ? null : new MinioClient(s3Host, s3AccessKey, s3SecretKey, useSSL);
+    }
+
+    @Value("${inbox.s3.host}")
+    public void setS3Host(String s3Host) {
+        this.s3Host = s3Host;
+    }
+
+    @Value("${inbox.s3.access-key}")
+    public void setS3AccessKey(String s3AccessKey) {
+        this.s3AccessKey = s3AccessKey;
+    }
+
+    @Value("${inbox.s3.secret-key}")
+    public void setS3SecretKey(String s3SecretKey) {
+        this.s3SecretKey = s3SecretKey;
+    }
+
+    @Value("${inbox.s3.use-ssl}")
+    public void setUseSSL(boolean useSSL) {
+        this.useSSL = useSSL;
     }
 
 }
--- src/main/java/se/nbis/lega/inbox/filesystem/S3FileSystemFactory.java
+++ src/main/java/se/nbis/lega/inbox/filesystem/S3FileSystemFactory.java
@@ -1,14 +1,16 @@
 package se.nbis.lega.inbox.filesystem;
 
+import com.amazonaws.Protocol;
 import com.upplication.s3fs.AmazonS3Factory;
 import com.upplication.s3fs.S3FileSystemProvider;
+import io.minio.MinioClient;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.sshd.common.file.FileSystemFactory;
 import org.apache.sshd.common.session.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.net.URI;
 import java.nio.file.FileSystem;
 import java.util.HashMap;
@@ -18,30 +20,40 @@ import java.util.Map;
 @Service
 public class S3FileSystemFactory implements FileSystemFactory {
 
-    private String s3URI;
+    private String s3Host;
     private String s3AccessKey;
     private String s3SecretKey;
+    private boolean useSSL;
+
+    private MinioClient minioClient;
 
     @Override
     public FileSystem createFileSystem(Session session) {
         String username = session.getUsername();
-        String root;
-        if (s3URI.endsWith(File.separator)) {
-            root = s3URI + username;
-        } else {
-            root = s3URI + File.separator + username;
+
+        try {
+            if (!minioClient.bucketExists(username)) {
+                minioClient.makeBucket(username);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e.getMessage(), e);
         }
-        log.info("Inbox initialized: {}", root);
+
+        String uri = String.format("s3://%s/%s", s3Host, username);
+        log.info("Inbox initialized: {}", uri);
+
         Map<String, String> env = new HashMap<>();
+        env.put(AmazonS3Factory.PROTOCOL, useSSL ? Protocol.HTTPS.name() : Protocol.HTTP.name());
         env.put(AmazonS3Factory.ACCESS_KEY, s3AccessKey);
         env.put(AmazonS3Factory.SECRET_KEY, s3SecretKey);
 
-        return new S3FileSystemProvider().newFileSystem(URI.create(root), env);
+        return new S3FileSystemProvider().newFileSystem(URI.create(uri), env);
     }
 
-    @Value("${inbox.s3.uri}")
-    public void setS3URI(String s3URI) {
-        this.s3URI = s3URI;
+    @Value("${inbox.s3.host}")
+    public void setS3Host(String s3Host) {
+        this.s3Host = s3Host;
     }
 
     @Value("${inbox.s3.access-key}")
@@ -52,6 +64,16 @@ public class S3FileSystemFactory implements FileSystemFactory {
     @Value("${inbox.s3.secret-key}")
     public void setS3SecretKey(String s3SecretKey) {
         this.s3SecretKey = s3SecretKey;
+    }
+
+    @Value("${inbox.s3.use-ssl}")
+    public void setUseSSL(boolean useSSL) {
+        this.useSSL = useSSL;
+    }
+
+    @Autowired(required = false)
+    public void setMinioClient(MinioClient minioClient) {
+        this.minioClient = minioClient;
     }
 
 }
