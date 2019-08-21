11e96f20595072bf483a9cfc4bcc65fa39c673c7
Dmytro Titov <dmytro.titov@gmail.com>
2019-08-21 13:03:32 +0200

    Separate deployment script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+  mvn clean
+  curl $TRYGGVE_CA --create-dirs -o ~/.docker/ca.pem
+  curl $TRYGGVE_CERT --create-dirs -o ~/.docker/cert.pem
+  curl $TRYGGVE_KEY --create-dirs -o ~/.docker/key.pem
+  docker stack rm INBOX
+  docker config rm CA.cert
+  docker config rm inbox.jks
+  docker config create CA.cert CA.cert
+  docker config create inbox.jks inbox.jks
+  export S3_ACCESS_KEY=$TEMP_S3_ACCESS_KEY
+  export S3_ENDPOINT=$TEMP_S3_ENDPOINT
+  export S3_SECRET_KEY=$TEMP_S3_SECRET_KEY
+  docker build -t nbisweden/ega-mina-inbox:latest .
+  docker stack deploy INBOX --compose-file docker-stack.yml
+fi
