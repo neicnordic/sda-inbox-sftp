bc52dd5935a774d213e2f63e7f70b78e833f2533
Dmytro Titov <dmytro.titov@gmail.com>
2019-08-21 16:40:23 +0200

    Remove volume on cleanup

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
   curl $TRYGGVE_CERT --create-dirs -o ~/.docker/cert.pem
   curl $TRYGGVE_KEY --create-dirs -o ~/.docker/key.pem
   docker stack rm INBOX
+  docker volume rm INBOX_inbox
   docker config rm CA.cert
   docker config rm inbox.jks
   docker config create CA.cert CA.cert
